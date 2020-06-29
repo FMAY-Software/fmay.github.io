bacabfd7134c7956756da0b0f65f62b72f745381
elilondon <44760400+elilondon@users.noreply.github.com>
2020-06-29 16:21:49 -0500

    Initial scrolling appBar

---
+++ src/containers/main.js
@@ -0,0 +1,11 @@
+import React from "react";
+import MainAppBar from "../components/toolbars/appBar";
+
+export default function MainPage() {
+  return (
+    <div>
+      <MainAppBar />
+      
+    </div>
+  );
+}
